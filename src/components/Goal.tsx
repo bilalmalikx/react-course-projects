f77a857c177b093c1381dd4272ac4df66cb9ba5b
Bilal Siddique Malik <outlookbilalmalik@gmail.com>
2025-09-02 15:29:07 +0500

    learned about PropsWithChildren

--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -1,11 +1,28 @@
-import type { ReactNode } from "react";
+// import type { ReactNode } from "react";
+
+// interface GoalProps {
+//   title: string;
+//   children: ReactNode; // ye har wo cheez ho sakti hai jo andar likhi jaaye
+// }
+
+// export default function Goal({ title, children }: GoalProps) {
+//   return (
+//     <article>
+//       <h1>{title}</h1>
+//       <div>{children}</div>
+//     </article>
+//   );
+// }
+import type { PropsWithChildren } from "react";
 
 interface GoalProps {
   title: string;
-  children: ReactNode; // ye har wo cheez ho sakti hai jo andar likhi jaaye
 }
 
-export default function Goal({ title, children }: GoalProps) {
+// PropsWithChildren GoalProps me children: ReactNode automatically add kar dega
+type GoalPropsWithChildren = PropsWithChildren<GoalProps>;
+
+export default function Goal({ title, children }: GoalPropsWithChildren) {
   return (
     <article>
       <h1>{title}</h1>
